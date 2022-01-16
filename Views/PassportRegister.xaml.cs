10e93580b0f0f45dc6d1fc85bde40c0dd2c84d62
haziq <haziq@DESKTOP-8CHBRDQ>
2022-01-16 20:13:38 +0500

    Add project files.

---
+++ Views/PassportRegister.xaml.cs
@@ -0,0 +1,42 @@
+﻿using PassportLogin.Models;
+using PassportLogin.Utils;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace PassportLogin.Views
+{
+    public sealed partial class PassportRegister : Page
+    {
+        private Account _account;
+
+        public PassportRegister()
+        {
+            InitializeComponent();
+        }
+
+        private async void RegisterButton_Click_Async(object sender, RoutedEventArgs e)
+        {
+            ErrorMessage.Text = "";
+
+            //In the real world you would normally validate the entered credentials and information before 
+            //allowing a user to register a new account. 
+            //For this sample though we will skip that step and just register an account if username is not null.
+
+            if (!string.IsNullOrEmpty(UsernameTextBox.Text))
+            {
+                //Register a new account
+                _account = AccountHelper.AddAccount(UsernameTextBox.Text);
+                //Register new account with Microsoft Passport
+                await MicrosoftPassportHelper.CreatePassportKeyAsync(_account.Username);
+                //Navigate to the Welcome Screen. 
+                Frame.Navigate(typeof(Welcome), _account);
+            }
+            else
+            {
+                ErrorMessage.Text = "Please enter a username";
+            }
+        }
+        
+    }
+}
---
+++ Views/Welcome.xaml.cs
@@ -0,0 +1,56 @@
+﻿using PassportLogin.Models;
+using PassportLogin.Utils;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace PassportLogin.Views
+{
+    public sealed partial class Welcome : Page
+    {
+        private Account _activeAccount;
+
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _activeAccount = (Account)e.Parameter;
+            if (_activeAccount != null)
+            {
+                UserNameText.Text = _activeAccount.Username;
+            }
+        }
+
+        private void Button_Restart_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(UserSelection));
+        }
+
+        private void Button_Forget_User_Click(object sender, RoutedEventArgs e)
+        {
+            MicrosoftPassportHelper.RemovePassportAccountAsync(_activeAccount);
+
+            // Remove it from the local accounts list and resave the updated list
+            AccountHelper.RemoveAccount(_activeAccount);
+
+            Debug.WriteLine("User " + _activeAccount.Username + " deleted.");
+
+            // Navigate back to UserSelection page.
+            Frame.Navigate(typeof(UserSelection));
+        }
+
+        private void Button_Shows(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Shows));
+        }
+
+        private void Button_Soon(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Soon));
+        }
+    }
+}
